55dfd7ee439faf0deca2ba85bfdce225ac3b77da
hao0632 <2640706787@qq.com>
2026-05-10 16:35:59 +0800

    更新图标为猫咪图标

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "hao的博客",
   description: "分享技术与生活",
+  icons: {
+    icon: "/cat.png",
+  },
 };
 
 export default function RootLayout({
